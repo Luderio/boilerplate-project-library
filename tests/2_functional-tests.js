455990aa6e2f8b05e0af14ade71c247c6ef006ca
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2022-01-22 12:54:30 +0800

    test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -22,8 +22,9 @@ suite('Functional Tests', function() {
   */
   test('#example Test GET /api/books', function(done){
      chai.request(server)
-      .get('/api/books/')
+      .get('/api/books')
       .end(function(err, res) {
+        assert.equal(res.status, 200);
         assert.isArray(res.body, 'response should be an array');
         assert.property(res.body[0], 'commentcount', 'Books in array should contain commentcount');
         assert.property(res.body[0], 'title', 'Books in array should contain title');
