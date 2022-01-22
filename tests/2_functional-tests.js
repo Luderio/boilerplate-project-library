a361a998d45747c6e8f4625d81928bcae593a41e
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2022-01-22 18:03:52 +0800

    Functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -14,6 +14,8 @@ const res = require('express/lib/response');
 
 chai.use(chaiHttp);
 
+let id1 = '';
+
 suite('Functional Tests', function() {
 
   /*
@@ -37,17 +39,34 @@ suite('Functional Tests', function() {
   * ----[END of EXAMPLE TEST]----
   */
 
-  /*suite('Routing tests', function() {
+  suite('Routing tests', function() {
 
 
     suite('POST /api/books with title => create book object/expect book object', function() {
       
       test('Test POST /api/books with title', function(done) {
-        //done();
+        chai.request(server)
+        .post('/api/books')
+        .send({"title": 'Test Title'})
+        .end(function(err, res) {
+          assert.isObject(res.body, 'response should be an object');
+          assert.property(res.body, '_id', 'the object property muts be equal to "_id"');
+          assert.property(res.body, 'title', 'the object property muts be equal to "title"');
+          id1 = res.body._id;
+          console.log('id 1 has been set as: ' + id1);
+          done();
+        });
       });
       
       test('Test POST /api/books with no title given', function(done) {
-        //done();
+        chai.request(server)
+        .post('/api/books')
+        .send({})
+        .end(function(err, res) {
+          assert.isString(res.body, 'response should be a String');
+          assert.equal(res.body, 'missing required field title', 'the object must contain "missing required field title"');
+          done();
+        });
       });
       
     });
@@ -56,29 +75,54 @@ suite('Functional Tests', function() {
     suite('GET /api/books => array of books', function(){
       
       test('Test GET /api/books',  function(done){
-        //done();
-      });      
-      
+        chai.request(server)
+        .get('/api/books')
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.isArray(res.body, 'response should be an array');
+          assert.property(res.body[0], '_id', 'Books in array should contain commentcount');
+          assert.property(res.body[0], 'title', 'Books in array should contain title');
+          assert.property(res.body[0], 'comments', 'Books in array should contain _id');
+          assert.property(res.body[0], 'commentcount', 'Books in array should contain comments');
+          done();
+        });
+      });
     });
 
 
     suite('GET /api/books/[id] => book object with [id]', function(){
       
       test('Test GET /api/books/[id] with id not in db',  function(done){
-        //done();
+        chai.request(server)
+        .get('/api/books/_id')
+        .query({"_id": 'Invalid id'})
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.isString(res.body, 'response should be a String');
+          assert.equal(res.body, 'no book exists', 'response must contain "no book exists"');
+          done();
+        });
       });
       
       test('Test GET /api/books/[id] with valid id in db',  function(done){
-        //done();
+        chai.request(server)
+        .get('/api/books/_id')
+        .query({"_id": id1})
+        .end(function(err, res) {
+          assert.isObject(res.body, 'response should be an Object');
+          assert.property(res.body, '_id', 'property should be equal to "_id"');
+          assert.property(res.body, 'title', 'property should be equal to "title"');
+          assert.property(res.body, 'comments', 'property should be equal to "comments"');
+          done();
+        });
       });
-      
     });
 
 
     suite('POST /api/books/[id] => add comment/expect book object with id', function(){
       
       test('Test POST /api/books/[id] with comment', function(done){
-        //done();
+        
       });
 
       test('Test POST /api/books/[id] without comment field', function(done){
@@ -103,6 +147,6 @@ suite('Functional Tests', function() {
 
     });
 
-  });*/
+  });
 
 });
